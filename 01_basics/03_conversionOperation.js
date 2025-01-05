d285afffb9418cb4071613d1c8bc684205a49c3a
nandani9479 <100612844+nandani9479@users.noreply.github.com>
2025-01-05 15:24:29 +0000

    commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score="33"
+// let score= null
+
+// let score= undefined
+
+let score= false
+let valueInNumber= Number(score);
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let scoreOne="33abc"
+// let valueScoreOne=Number(scoreOne);
+// console.log(typeof scoreOne);
+// console.log(typeof valueScoreOne);
+// console.log(valueScoreOne);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1 , false =>0
+
+// let isLoggedIn=1 
+
+// let isLoggedIn=""
+
+// let isLoggedIn="Nandani"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
